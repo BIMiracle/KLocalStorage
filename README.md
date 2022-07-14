8c961cc46f5550476c446b1c3c1da69a5c8d8e74
The_wish <18730979+tryEvething@users.noreply.github.com>
2022-07-14 14:50:55 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # KLocalStorage
-enhanced localStorage(加强版localStorage)
+
+enhanced localStorage(加强版 localStorage)
+
+# INSTALL(安装)
+
+```bash
+npm install KLocalStorage
+```
+
+# USAGE(使用)
+
+For Vue Project in main.js add
+
+```js
+import 'KLocalStorage'
+```
+
+create a `storage.config.js`
+
+```js
+module.exports = {
+	storagePrefix: '',
+	localStorageWhiteList: [],
+}
+```
+
+`storagePrefix`: addPrefix (在普通 key 上添加前缀)
+`localStorageWhiteList`: localStorage.clean() will skip array (使用 localStorage.clean()时会跳过里面的值)
+
+then you can use it as native localStorage
+
+```js
+localStorage.a = '1'
+localStorage.setItem('a', '2')
+localStorage.a
+localStorage.getItem('a')
+localStorage.removeItem('a')
+localStorage.clean() // only clean add storagePrefix and skip localStorageWhiteList (只会清除添加了storagePrefix值的前缀,会跳过localStorageWhiteList白名单里的值)
+```
---
+++ index.js
@@ -0,0 +1,134 @@
+const setting = require('./storage.config')
+
+if (setting) {
+	if (!setting.storagePrefix) {
+		throw new Error(`KLocalStorage: storagePrefix not found`)
+	}
+	if (!setting.localStorageWhiteList) {
+		throw new Error(`KLocalStorage: localStorageWhiteList not found`)
+	}
+}else{
+	throw new Error(`storage.config.js setting not found`)
+}
+
+let _localStorageSave
+
+function addPrefix (key) {
+	if (key.indexOf(`${setting.storagePrefix}_`) === 0) {
+		return key
+	}
+	return `${setting.storagePrefix}_${key}`
+}
+
+class Storage {
+	constructor() {
+		const arr = Object.keys(_localStorageSave)
+		this._obj = {}
+		for (let i = 0, len = arr.length; i < len; i++) {
+			const key = arr[i];
+			this._obj[key] = _localStorageSave[key]
+		}
+	}
+	setItem (key, value) {
+		if (!key) {
+			throw new Error(`localStorage setItem: key is null`)
+		}
+		if (!value) {
+			console.error(`localStorage setItem: key:${key}, value is null`)
+			return
+			// throw new Error(`localStorage setItem: key:${key}, value is null`)
+		}
+
+		const v = JSON.stringify(value)
+		_localStorageSave.setItem(addPrefix(key), v)
+
+		this._obj[addPrefix(key)] = v
+	}
+	getItem (key) {
+		if (!key) {
+			throw new Error(`localStorage getItem: key is null`)
+		}
+		let v = this._obj[addPrefix(key)]
+
+		try {
+			v = JSON.parse(v)
+			// eslint-disable-next-line no-empty
+		} catch (error) {
+		}
+		return v
+	}
+	removeItem (key) {
+		if (!key) {
+			throw new Error(`localStorage removeItem: key is null`)
+		}
+		delete this._obj[addPrefix(key)]
+		_localStorageSave.removeItem(addPrefix(key))
+	}
+	clear () {
+		const perfix = `${setting.storagePrefix}_`
+		const arr = Object.keys(_localStorageSave)
+		for (let i = 0, len = arr.length; i < len; i++) {
+			const item = arr[i];
+			if (item.indexOf(perfix) == 0 && !setting?.localStorageWhiteList?.some(c => perfix + c === item)) {
+				_localStorageSave.removeItem(item)
+				delete this._obj[item]
+			}
+		}
+	}
+	toString () {
+		return this._obj
+	}
+	[Symbol.toPrimitive] (hint) {
+		if (hint === 'number') {
+			return NaN;
+		}
+		return '[object Storage]'
+	}
+}
+
+(function changeLocalStorage () {
+	_localStorageSave = window.localStorage
+
+	const s = new Storage()
+
+	let p = new Proxy(s, {
+		get: function(target, property, receiver) {
+			// console.warn(property);
+			switch (property) {
+				case "setItem":
+					return Reflect.get(target, property, receiver);
+				case "getItem":
+					// return s.getItem(property)
+					return Reflect.get(target, property, receiver);
+				case "removeItem":
+					// return target.removeItem(property)
+					return Reflect.get(target, property, receiver);
+				case "clear":
+					return Reflect.get(target, property, receiver);
+				case "toString":
+					return Reflect.get(target, property, receiver);
+				case "_obj":
+					return Reflect.get(target, property, receiver);
+				default:
+					{
+						if (typeof (property) == 'symbol') {
+							return Reflect.get(target, property, receiver);
+						}
+						return target.getItem(property)
+					}
+			}
+		},
+		set: function(target, property, value, receiver) {
+			target.setItem(property, value)
+			return true
+			// return Reflect.set(target, property, value, receiver);
+		},
+	});
+
+	Object.defineProperty(window, 'localStorage', {
+		value: p,
+		// configurable: false,
+		// enumerable: false,
+		writable: false
+	});
+})()
